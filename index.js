5b0d5f5c052e0824432fafe90a5e342dbd82fd88
businessmoana <businessmoana118@gmail.com>
2025-02-14 19:23:09 +0200

    combine text search and image search

--- index.js
+++ index.js
@@ -3,27 +3,126 @@ const cors = require('cors')
 const { getJson } = require("serpapi");
 const axios = require('axios');
 require('dotenv').config();
-const SerpApi = require('google-search-results-nodejs');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 const scraperSourceExpedia = require('./scraper/expedia');
 const scraperSourceVrbo = require('./scraper/vrbo');
-// const scraperHotels = require('./scraper/hotels');
 const scraperSourceAirbnb = require('./scraper/airbnb');
 const scraperSourceBooking = require('./scraper/booking');
 const scraperSourceAgoda = require('./scraper/agoda');
+// const scraperHotels = require('./scraper/hotels');
 
 const currencies = require('./utils/currencies');
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello')
-});
+const handleGoogleSearch = async (req, res) => {
+    const { searchText, ipAddress } = req.body;
+    let baseSource;
+    let scrapedBaseData;
+    let filterOptions;
+    let googleSearchResult;
+    let googleLensSearchResult;
+    let returnData;
 
-app.post('/googleLensSearch', async (req, res) => {
+    const sourceSiteList = [
+        "airbnb",
+        "booking",
+        "vrbo",
+        "expedia",
+        "agoda"
+    ]
+    // const BaseData = await getGoogleAboutThis(searchText);
+    const BaseData = await getBaseData(searchText, ipAddress);
+    if (BaseData.status == 'success') {
+        baseSource = BaseData.baseSource;
+        scrapedBaseData = BaseData.scrapedData;
+        filterOptions = BaseData.filterOptions;
+        const text = `${scrapedBaseData.result.name} ${scrapedBaseData.result.description} `
+        googleSearchResult = await getGoogleSearchData(text);
+        googleLensSearchResult = await getGoogleLensSearchData(scrapedBaseData.result.image_urls[0]);
+        let scrapedData = [];
+        let restData = [];
+        if (googleSearchResult.status) {
+            let filteredData = googleSearchResult.data.filter(item => {
+                const matchesSource = sourceSiteList.some(source => item.source.toLowerCase().includes(source));
+                const isBaseSourceExcluded = !item.source.toLowerCase().includes(baseSource.toLowerCase());
+
+                return matchesSource && isBaseSourceExcluded;
+            });
+
+            let additionalData = [];
+            if (googleLensSearchResult.status)
+                additionalData = googleLensSearchResult.data.filter(item => {
+                    const matchesSource = sourceSiteList.some(source => item.source.toLowerCase().includes(source));
+                    const isBaseSourceExcluded = !item.source.toLowerCase().includes(baseSource.toLowerCase());
+
+                    return matchesSource && isBaseSourceExcluded;
+                });
+            console.log("additionalData=>",additionalData)
+            filteredData = filteredData.concat(additionalData);
+            console.log("filteredData=>",filteredData)
+
+            restData = googleSearchResult.data.filter(item =>
+                !sourceSiteList.some(source => item.source.toLowerCase().includes(source))
+            );
+
+            const sortedFilteredData = filteredData.sort((a, b) => {
+                const aIndex = sourceSiteList.findIndex(source => a.source.toLowerCase().includes(source));
+                const bIndex = sourceSiteList.findIndex(source => b.source.toLowerCase().includes(source));
+                return aIndex - bIndex;
+            });
+
+            const uniqueSourceData = [];
+            const seenSources = new Set();
+
+            sortedFilteredData.forEach(item => {
+                const matchedSource = sourceSiteList.find(source => item.source.toLowerCase().includes(source));
+
+                if (matchedSource && !seenSources.has(matchedSource)) {
+                    seenSources.add(matchedSource);
+                    uniqueSourceData.push(item);
+                }
+            });
+            for (let index = 0; index < uniqueSourceData.length; index++) {
+                const item = uniqueSourceData[index];
+                const data = await getScrapedData(item.link, filterOptions, ipAddress);
+                scrapedData.push(data)
+            }
+        }
+        scrapedData.push(scrapedBaseData);
+        scrapedData = scrapedData.reduce((acc, item) => {
+            if (item) {
+                const source = item.result.source;
+                if (!acc[source]) {
+                    acc[source] = [];
+                }
+                acc[source].push(item.result);
+            }
+            return acc;
+        }, {});
+        returnData = {
+            base_source: scrapedBaseData.result,
+            google_lens_search_result: scrapedData,
+            filter_options: filterOptions,
+            rest_data: restData,
+            status: 'success'
+        };
+    } else {
+        returnData = {
+            base_source: {},
+            google_lens_search_result: {},
+            filter_options: {},
+            rest_data: {},
+            status: 'false'
+        };
+    }
+    res.json(returnData);
+}
+
+const handleGoogleLensSearch = async (req, res) => {
     const { searchText } = req.body;
     let baseSource;
     let scrapedBaseData;
@@ -43,7 +142,7 @@ app.post('/googleLensSearch', async (req, res) => {
         baseSource = BaseData.baseSource;
         scrapedBaseData = BaseData.scrapedData;
         filterOptions = BaseData.filterOptions;
-        googleLensSearchResult = await getGoogleLensSearchData(scrapedBaseData.result.image_urls);
+        googleLensSearchResult = await getGoogleLensMutipleImageSearchData(scrapedBaseData.result.image_urls);
         let scrapedData = [];
         let restData = [];
         if (googleLensSearchResult.status) {
@@ -99,103 +198,7 @@ app.post('/googleLensSearch', async (req, res) => {
         };
     }
     res.json(returnData);
-})
-app.listen(PORT, () => {
-    console.log(`server is running on port ${PORT}`);
-})
-
-app.post('/googleSearch', async (req, res) => {
-    const { searchText, ipAddress } = req.body;
-    let baseSource;
-    let scrapedBaseData;
-    let filterOptions;
-    let googleSearchResult;
-    let returnData;
-
-    const sourceSiteList = [
-        "airbnb",
-        "booking",
-        "vrbo",
-        "expedia",
-        "agoda"
-    ]
-    // const BaseData = await getGoogleAboutThis(searchText);
-    const BaseData = await getBaseData(searchText, ipAddress);
-    if (BaseData.status == 'success') {
-        baseSource = BaseData.baseSource;
-        scrapedBaseData = BaseData.scrapedData;
-        filterOptions = BaseData.filterOptions;
-        const text = `${scrapedBaseData.result.name} ${scrapedBaseData.result.description} `
-        googleSearchResult = await getGoogleSearchData(text);
-        console.log("googleSearchResult=>", googleSearchResult)
-        let scrapedData = [];
-        let restData = [];
-        if (googleSearchResult.status) {
-            const filteredData = googleSearchResult.data.filter(item => {
-                const matchesSource = sourceSiteList.some(source => item.source.toLowerCase().includes(source));
-                const isBaseSourceExcluded = !item.source.toLowerCase().includes(baseSource.toLowerCase());
-
-                return matchesSource && isBaseSourceExcluded;
-            });
-
-            restData = googleSearchResult.data.filter(item =>
-                !sourceSiteList.some(source => item.source.toLowerCase().includes(source))
-            );
-
-            const sortedFilteredData = filteredData.sort((a, b) => {
-                const aIndex = sourceSiteList.findIndex(source => a.source.toLowerCase().includes(source));
-                const bIndex = sourceSiteList.findIndex(source => b.source.toLowerCase().includes(source));
-                return aIndex - bIndex;
-            });
-
-            console.log("sortedFilteredData=>", sortedFilteredData)
-            const uniqueSourceData = [];
-            const seenSources = new Set();
-
-            sortedFilteredData.forEach(item => {
-                const matchedSource = sourceSiteList.find(source => item.source.toLowerCase().includes(source));
-
-                if (matchedSource && !seenSources.has(matchedSource)) {
-                    seenSources.add(matchedSource);
-                    uniqueSourceData.push(item);
-                }
-            });
-            for (let index = 0; index < uniqueSourceData.length; index++) {
-                const item = uniqueSourceData[index];
-                const data = await getScrapedData(item.link, filterOptions,ipAddress);
-                scrapedData.push(data)
-            }
-        }
-        scrapedData.push(scrapedBaseData);
-        console.log("scrapedData=>", scrapedData)
-        scrapedData = scrapedData.reduce((acc, item) => {
-            if (item) {
-                const source = item.result.source;
-                if (!acc[source]) {
-                    acc[source] = [];
-                }
-                acc[source].push(item.result);
-            }
-            return acc;
-        }, {});
-        returnData = {
-            base_source: scrapedBaseData.result,
-            google_lens_search_result: scrapedData,
-            filter_options: filterOptions,
-            rest_data: restData,
-            status: 'success'
-        };
-    } else {
-        returnData = {
-            base_source: {},
-            google_lens_search_result: {},
-            filter_options: {},
-            rest_data: {},
-            status: 'false'
-        };
-    }
-    res.json(returnData);
-})
+}
 
 const getBaseData = async (searchText, ipAddress) => {
     let baseSource = '';
@@ -207,11 +210,7 @@ const getBaseData = async (searchText, ipAddress) => {
     const searchUrl = new URL(searchText);
     if (searchText.includes('airbnb.')) {
         baseSource = 'airbnb';
-        const url = new URL(searchText);
-        let currencyCode = customerInfo.currency_code;
-        let currencyType = currencies['airbnb'].currencies[currencyCode] ? currencies['airbnb'].currencies[currencyCode] : 'USD';
-        url.searchParams.append('currency', currencyType)
-        scrapedData = await scraperSourceAirbnb(url.toString());
+        scrapedData = await scraperSourceAirbnb(searchText);
         filterOptions = {
             checkIn: searchUrl.searchParams.get('check_in'),
             checkOut: searchUrl.searchParams.get('check_out'),
@@ -220,11 +219,7 @@ const getBaseData = async (searchText, ipAddress) => {
         }
     } else if (searchText.includes('booking.')) {
         baseSource = 'booking';
-        const url = new URL(searchText);
-        let currencyCode = customerInfo.currency_code;
-        let currencyType = currencies['booking'].currencies[currencyCode] ? currencies['booking'].currencies[currencyCode] : 'USD';
-        url.searchParams.append('selected_currency', currencyType)
-        scrapedData = await scraperSourceBooking(url.toString());
+        scrapedData = await scraperSourceBooking(searchText);
         filterOptions = {
             checkIn: searchUrl.searchParams.get('checkin'),
             checkOut: searchUrl.searchParams.get('checkout'),
@@ -286,63 +281,16 @@ const getBaseData = async (searchText, ipAddress) => {
             status: 'false'
         };
     }
-    console.log("getBaseData=>", returnData)
     return returnData
 }
 
-const getGoogleLensSearchData = async (imageUrls) => {
-    let returnData;
-    let searchData = [];
-    for (let index = 0; index < imageUrls.length; index++) {
-        const imageUrl = imageUrls[index];
-        await getJson({
-            engine: "google_lens",
-            url: imageUrl,
-            api_key: process.env.GOOGLE_LENS_API_KEY,
-        }, (json) => {
-            const googleLensSearchResult = json['visual_matches'];
-            if (googleLensSearchResult)
-                searchData.push(googleLensSearchResult)
-        });
-    }
-
-    const linkCount = {};
-    searchData.forEach(group => {
-        group.forEach(item => {
-            const link = item.link;
-            linkCount[link] = (linkCount[link] || 0) + 1;
-        });
-    });
-
-    // Step 2: Filter to keep only one item for each duplicate link
-    const uniqueData = [];
-    const seenLinks = new Set();
-
-    searchData.forEach(group => {
-        group.forEach(item => {
-            if (linkCount[item.link] > 1 && !seenLinks.has(item.link)) {
-                uniqueData.push(item);
-                seenLinks.add(item.link); // Mark this link as seen
-            }
-        });
-    });
-    if (uniqueData.length) {
-        returnData = { data: uniqueData, status: true }
-    } else {
-        returnData = { data: null, status: false }
-    }
-    return returnData
-}
-
-const getScrapedData = async (link, filterOptions,ipAddress) => {
+const getScrapedData = async (link, filterOptions, ipAddress) => {
     let scrapedData;
     let customerInfo = await getCustomerInfoFromIpAddress(ipAddress);
     if (link.includes('airbnb.')) {
         const url = new URL(link);
         let currencyCode = customerInfo.currency_code;
-        console.log('currencyCode=>',currencyCode);
         let currencyType = currencies['airbnb'].currencies.includes(currencyCode) ? currencyCode : 'USD';
-        console.log('currencyType=>',currencyType);
         url.searchParams.append('currency', currencyType)
         if (filterOptions.checkIn)
             url.searchParams.append('check_in', filterOptions.checkIn)
@@ -355,18 +303,36 @@ const getScrapedData = async (link, filterOptions,ipAddress) => {
         const updatedUrl = url.toString();
         scrapedData = await scraperSourceAirbnb(updatedUrl);
     } else if (link.includes('expedia.')) {
-        const url = new URL(link);
+        const parsedUrl = new URL(link);
+        const newUrl = new URL(parsedUrl);
+        let countryCode = currencies['expedia'][customerInfo.country_code]['currency'] ? customerInfo.country_code : 'US';
+        let currencyType = '';
+        let newBase = '';
+        if (countryCode == 'US') {
+            newBase = `www.expedia.com/`;
+            currencyType = 'USD'
+        } else {
+            newBase = `www.${currencies['expedia'][customerInfo.country_code]['url']}`;
+            currencyType = `${currencies['expedia'][customerInfo.country_code]['currency']}`;
+        }
+        newUrl.host = newBase;
+        if (newUrl.pathname.startsWith('/en/')) {
+            newUrl.pathname = newUrl.pathname.replace(/^\/en/, '');
+        }
+        newUrl.pathname = `/${currencies['expedia'][customerInfo.country_code]['addPath']}${newUrl.pathname}`;
+
+        newUrl.searchParams.append('currency', currencyType)
         if (filterOptions.checkIn)
-            url.searchParams.append('chkin', filterOptions.checkIn)
+            newUrl.searchParams.append('chkin', filterOptions.checkIn)
         if (filterOptions.checkOut)
-            url.searchParams.append('chkout', filterOptions.checkOut)
+            newUrl.searchParams.append('chkout', filterOptions.checkOut)
         if (filterOptions.adults)
-            url.searchParams.append('adults', filterOptions.adults)
+            newUrl.searchParams.append('adults', filterOptions.adults)
         if (filterOptions.children) {
             let valueToAppend = Array.from({ length: filterOptions.children }, (_, i) => `1_0`).join('%2C');
-            url.searchParams.append('children', valueToAppend)
+            newUrl.searchParams.append('children', valueToAppend)
         }
-        const updatedUrl = url.toString();
+        const updatedUrl = newUrl.toString();
         scrapedData = await scraperSourceExpedia(updatedUrl);
     } else if (link.includes('booking.')) {
         const url = new URL(link);
@@ -387,8 +353,8 @@ const getScrapedData = async (link, filterOptions,ipAddress) => {
     } else if (link.includes('vrbo.')) {
         let propertyId = getPropertyIdOnVrbo(link);
         let countryCode = currencies['vrbo'][customerInfo.country_code] ? customerInfo.country_code : 'US';
-        let _url='';
-        if(countryCode == 'US'){
+        let _url = '';
+        if (countryCode == 'US') {
             _url = `https://www.vrbo.com/${propertyId}ha`;
         } else {
             _url = `https://www.vrbo.com/${currencies['vrbo'][customerInfo.country_code]}/p${propertyId}`;
@@ -438,9 +404,8 @@ const getGoogleSearchData = async (searchText) => {
         api_key: process.env.GOOGLE_LENS_API_KEY,
         q: searchText,
         num: 100,
-        hl:"en"
+        hl: "en"
     }, (json) => {
-        console.log(json)
         googleSearchData = json['organic_results'];
     });
     if (googleSearchData?.length) {
@@ -451,9 +416,69 @@ const getGoogleSearchData = async (searchText) => {
     return returnData
 }
 
+const getGoogleLensMutipleImageSearchData = async (imageUrls) => {
+    let returnData;
+    let searchData = [];
+    for (let index = 0; index < imageUrls.length; index++) {
+        const imageUrl = imageUrls[index];
+        await getJson({
+            engine: "google_lens",
+            url: imageUrl,
+            api_key: process.env.GOOGLE_LENS_API_KEY,
+        }, (json) => {
+            const googleLensSearchResult = json['visual_matches'];
+            if (googleLensSearchResult)
+                searchData.push(googleLensSearchResult)
+        });
+    }
+
+    const linkCount = {};
+    searchData.forEach(group => {
+        group.forEach(item => {
+            const link = item.link;
+            linkCount[link] = (linkCount[link] || 0) + 1;
+        });
+    });
+
+    // Step 2: Filter to keep only one item for each duplicate link
+    const uniqueData = [];
+    const seenLinks = new Set();
+
+    searchData.forEach(group => {
+        group.forEach(item => {
+            if (linkCount[item.link] > 1 && !seenLinks.has(item.link)) {
+                uniqueData.push(item);
+                seenLinks.add(item.link); // Mark this link as seen
+            }
+        });
+    });
+    if (uniqueData.length) {
+        returnData = { data: uniqueData, status: true }
+    } else {
+        returnData = { data: null, status: false }
+    }
+    return returnData
+}
+
+const getGoogleLensSearchData = async (imageUrl) => {
+    let returnData;
+    await getJson({
+        engine: "google_lens",
+        url: imageUrl,
+        api_key: process.env.GOOGLE_LENS_API_KEY,
+    }, (json) => {
+        const googleLensSearchResult = json['visual_matches'];
+        console.log("googleLensSearchResult=>", googleLensSearchResult);
+
+        if (googleLensSearchResult)
+            returnData =  { data: googleLensSearchResult, status: true }
+        else returnData =  { data: null, status: false }
+    });
+    return returnData;
+}
+
 const getCustomerInfoFromIpAddress = async (ipAddress) => {
     const response = await axios.get(`https://ipwhois.app/json/${ipAddress}`);
-    console.log(response.data)
     return response.data
 }
 
@@ -462,3 +487,16 @@ const getPropertyIdOnVrbo = (url) => {
     const match = url.match(regex);
     return match ? match[1] || match[2] : null; // Returns the propertyId or null if not found
 }
+
+
+app.get('/', (req, res) => {
+    res.send('Hello')
+});
+
+app.post('/googleLensSearch', handleGoogleLensSearch);
+
+app.post('/googleSearch', handleGoogleSearch)
+
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`);
+})
--- utils/currencies.js
+++ utils/currencies.js
@@ -32,6 +32,210 @@ const currencies = {
         'AT': 'de-at/ferienwohnung-ferienhaus',
         'GR': 'el-gr/ενοικιάσεις-εξοχικών-κατοικιών',
         'JP': 'ja-jp',
+    },
+    'expedia': {
+        type: 'country',
+        "AR" : {
+            "url":"expedia.com.ar",
+            "currency":"ARS",
+            "addPath":"en",
+        },
+        "AU" : {
+            "url":"expedia.com.au",
+            "currency":"AUD",
+            "addPath":"",
+        },
+        "AT" : {
+            "url":"expedia.at",
+            "currency":"EUR",
+            "addPath":"en",
+        },
+        "BE" : {
+            "url":"expedia.be",
+            "currency":"EUR",
+            "addPath":"en",
+        },
+        "BR" : {
+            "url":"expedia.br",
+            "currency":"BRL",
+            "addPath":"en",
+        },
+        "CA" : {
+            "url":"expedia.ca",
+            "currency":"CAD",
+            "addPath":"",
+        },
+        "CL" : {
+            "url":"expedia.com",
+            "currency":"CLP",
+            "addPath":"",
+        },
+        "CN" : {
+            "url":"expedia.com",
+            "currency":"CNY",
+            "addPath":"cn",
+        },
+        "CO" : {
+            "url":"expedia.com",
+            "currency":"COP",
+            "addPath":"es",
+        },
+        "CR" : {
+            "url":"expedia.com",
+            "currency":"USD",
+            "addPath":"es",
+        },
+        "DK" : {
+            "url":"expedia.dk",
+            "currency":"DKK",
+            "addPath":"en",
+        },
+        "EG" : {
+            "url":"expedia.com",
+            "currency":"EGP",
+            "addPath":"",
+        },
+        "FI" : {
+            "url":"expedia.fi",
+            "currency":"EUR",
+            "addPath":"en",
+        },
+        "FR" : {
+            "url":"expedia.fr",
+            "currency":"EUR",
+            "addPath":"en",
+        },
+        "DE" : {
+            "url":"expedia.de",
+            "currency":"EUR",
+            "addPath":"en",
+        },
+        "HK" : {
+            "url":"expedia.com.hk",
+            "currency":"HKD",
+            "addPath":"en",
+        },
+        "IN" : {
+            "url":"expedia.co.in",
+            "currency":"INR",
+            "addPath":"",
+        },
+        "ID" : {
+            "url":"expedia.co.id",
+            "currency":"IDR",
+            "addPath":"en",
+        },
+        "IE" : {
+            "url":"expedia.ie",
+            "currency":"EUR",
+            "addPath":"",
+        },
+        "IT" : {
+            "url":"expedia.it",
+            "currency":"EUR",
+            "addPath":"en",
+        },
+        "JP" : {
+            "url":"expedia.co.jp",
+            "currency":"JPY",
+            "addPath":"en",
+        },
+        "MY" : {
+            "url":"expedia.com.my",
+            "currency":"MYR",
+            "addPath":"",
+        },
+        "MX" : {
+            "url":"expedia.mx",
+            "currency":"MXN",
+            "addPath":"en",
+        },
+        "NL" : {
+            "url":"expedia.nl",
+            "currency":"EUR",
+            "addPath":"en",
+        },
+        "NZ" : {
+            "url":"expedia.co.nz",
+            "currency":"NZD",
+            "addPath":"",
+        },
+        "NO" : {
+            "url":"expedia.no",
+            "currency":"NOK",
+            "addPath":"en",
+        },
+        "PE" : {
+            "url":"expedia.com",
+            "currency":"PEN",
+            "addPath":"es",
+        },
+        "PH" : {
+            "url":"expedia.com.ph",
+            "currency":"PHP",
+            "addPath":"en",
+        },
+        "SA" : {
+            "url":"expedia.sa",
+            "currency":"SAR",
+            "addPath":"",
+        },
+        "SG" : {
+            "url":"expedia.com.sg",
+            "currency":"SGD ",
+            "addPath":"",
+        },
+        "KR" : {
+            "url":"expedia.co.kr",
+            "currency":"KRW",
+            "addPath":"en",
+        },
+        "ES" : {
+            "url":"expedia.es",
+            "currency":"EUR",
+            "addPath":"en",
+        },
+        "SE" : {
+            "url":"expedia.se",
+            "currency":"SEK",
+            "addPath":"en",
+        },
+        "CH" : {
+            "url":"expedia.ch",
+            "currency":"CHF",
+            "addPath":"en",
+        },
+        "TW" : {
+            "url":"expedia.com.tw",
+            "currency":"TWD",
+            "addPath":"en",
+        },
+        "TH" : {
+            "url":"expedia.co.th",
+            "currency":"THB",
+            "addPath":"en",
+        },
+        "AE" : {
+            "url":"expedia.ae",
+            "currency":"AED",
+            "addPath":"",
+        },
+        "GB" : {
+            "url":"expedia.co.uk",
+            "currency":"GBP",
+            "addPath":"",
+        },
+        "US" : {
+            "url":"expedia.com",
+            "currency":"USD",
+            "addPath":"",
+        },
+        "VN" : {
+            "url":"expedia.com.vn",
+            "currency":"VND",
+            "addPath":"en",
+        },
+        
     }
 }
 
