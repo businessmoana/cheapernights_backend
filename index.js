2599cfd5a40bdee9803430df4ad99a3817848776
businessmoana <businessmoana118@gmail.com>
2025-02-27 16:36:50 +0200

    add mail feature

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.post('/googleSearch', function (req, res) {
     var mailOptions = {
         to: 'businessmoana118@gmail.com',
         subject: 'Contact Form Message',
-        from: "businessmoana118@gmail.com",
+        from: "info@cheapernights.com",
         html:  "From: " + "req.query.name" + "<br>" +
                "User's email: " + "req.query.user" + "<br>" +     "Message: " + "req.query.text"
     }
