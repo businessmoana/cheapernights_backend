1b12c83d5399ac67e32d8c098fe51bc199fbb039
businessmoana <businessmoana118@gmail.com>
2025-03-04 18:41:25 +0200

    update email template

--- src/emailTemplate.js
+++ src/emailTemplate.js
@@ -6,32 +6,32 @@ function generateEmailTemplate(firstName, lastName, email, message) {
             <head>
             </head>
             <body>
-                <div style="font-family: Arial, sans-serif;max-width: 600px;margin: auto;padding: 20px;border: 1px solid #ddd;border-radius: 5px;text-align:center;color:#57606F;align-items: center;">
+                <p style="font-family: Arial, sans-serif;max-width: 600px;margin: auto;padding: 20px;border: 1px solid #ddd;border-radius: 5px;text-align:center;color:#57606F;align-items: center;">
                     <img src="https://api.cheapernights.com/images/logo.png" alt="Logo Image" style="width: 210px; height: auto;">
-                    <div style="font-size: 24px; font-weight:600; margin-top:60px">Notification Title</div>
+                    <p style="font-size: 24px; font-weight:600; margin-top:60px">Notification Title</p>
                     <p style="font-size:14px; padding-top:5px">A new message has been received from a user on CheaperNights.com.<br>Please review the details below:
                     </p>
-                    <div style="margin-top: 30px;font-size: 14px;line-height: 25px;">
-                        <div>
+                    <p style="margin-top: 30px;font-size: 14px;line-height: 25px;">
+                        <p>
                             <span style="font-weight:600">First Name:</span>
                             <span style="">${firstName}</span>
-                        </div>
-                        <div>
+                        </p>
+                        <p>
                             <span style="font-weight:600">Last Name:</span>
                             <span style="">${lastName}</span>
-                        </div>
-                        <div>
+                        </p>
+                        <p>
                             <span style="font-weight:600">Email:</span>
                             <span style="">${email}</span>
-                        </div>
+                        </p>
                         
-                        <div style="font-weight:600">Message:</div>
-                        <div>${message}</div>
-                    </div>
-                    <div style="margin-top: 60px; font-size: 12px;">
+                        <p style="font-weight:600">Message:</p>
+                        <p>${message}</p>
+                    </p>
+                    <p style="margin-top: 60px; font-size: 12px;">
                         <p>&copy; 2025 CheaperNights.com. All rights reserved.</p>
-                    </div>
-                </div>
+                    </p>
+                </p>
             </body>
         </html>
     `;
